1dcf003b43f5e6ef35e9b8a9b0a4cad73f6a70de
2005964 <hayeon_1027@naver.com>
2021-12-06 16:53:47 +0900

    add static_root

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b0zv+n%2upofwsa13535q2j^4a4*rhnq^tsvt#glieqsyxd-a8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
