8ec7676b06f7e7ac25823143670aad03b96027c0
2005964 <hayeon_1027@naver.com>
2021-12-19 06:05:18 +0900

    add static -root

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
@@ -42,3 +42,8 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
 
     return render(request, 'blog/post_edit.html', {'form':form})
+
+def post_delete(request, pk):
+    post = Post.objects.get(id=pk)
+    post.delete()
+    return redirect('/blog/')
